e1679ca409169fb160a373d83d24d6a9c7645b41
mpmckenna8 <mpmckenna8@me.com>
2019-07-16 21:54:43 -0700

    a module for getting all the files in a directory

---
+++ photo_viewer/pictureArray.js
@@ -0,0 +1,30 @@
+// a function to take the name of a directory as a arg and return an array of all the jpgs in theres
+
+
+let fs = require('fs');
+
+
+//let picsync = fs.readdirSync('../ride' )
+
+//console.log('getting pictures', picsync)
+let picasync = function( dir, cb ) {
+
+  let pics_array = [];
+
+  fs.readdir('../ride', (err, files ) => {
+
+    if(err) {
+      console.log('err getting the pictures list')
+    }
+
+    console.log(files)
+    cb(files)
+    return files
+  })
+
+}
+
+//picasync('../ride', () => {})
+
+
+module.exports = picasync
