3254afc2efd35ac540d333bbb85f1e6cb59bb098
mpmckenna8 <mpmckenna8@me.com>
2019-09-05 11:25:18 -0700

    Added a filter to take dot files out of the picture array thing.  probably actually interchangable with the directory one but whateves.

--- photo_viewer/pictureArray.js
+++ photo_viewer/pictureArray.js
@@ -1,20 +1,23 @@
 // a function to take the path of a directory as a arg and return an array of all the jpgs in theres
 
-
 let fs = require('fs');
 
-//let picsync = fs.readdirSync('../ride' )
 
-//console.log('getting pictures', picsync)
 let picasync = function( dir, cb ) {
 
   let pics_array = [];
+  console.log('dir = ', dir.slice(1,))
 
   fs.readdir(dir, (err, files ) => {
 
     if(err) {
-      console.log('err getting the pictures list')
+      console.log('err getting the pictures list', err)
+      cb([])
+
     }
+
+    files = files.filter( (file) => file[0] !== '.')
+
 
   //  console.log(files)
     cb(files)
