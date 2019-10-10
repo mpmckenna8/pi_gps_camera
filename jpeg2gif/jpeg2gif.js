1e1e2c6735bbc050eae09d3e5276973698133f4e
mpmckenna8 <mpmckenna8@me.com>
2019-10-10 09:28:38 -0700

    Fixed it up so the photo is a little cropped for gifs but should work on fine tuning that.

--- jpeg2gif/jpeg2gif.js
+++ jpeg2gif/jpeg2gif.js
@@ -2,17 +2,40 @@
 
 var getPixels = require('get-pixels')
 var GifEncoder = require('gif-encoder');
-var gif = new GifEncoder(720, 480);
 
-var file = require('fs').createWriteStream('./img.gif');
-var pics = ['../photo_viewer/public/pics/09-08_gg_park/1567951224.jpg', '../photo_viewer/public/pics/09-08_gg_park/1567951229.jpg', '../photo_viewer/public/pics/09-08_gg_park/1567951233.jpg'];
+var get_pics = require('./get_pictures_array.js')
+var rotate_pic = require('./rotate_picture.js');
+var gif = new GifEncoder( 482, 600, 4 );
+
+
+let num_pics = process.argv[4] || 25;
+
+let gif_name =  process.argv[5] || "giffer.gif";
+
+let first_pic_name = process.argv[3] || "1570246713.jpg";
+
+let pic_fold = process.argv[2] || '10-04_sf-bike-party';
+let pics_path = './photo_viewer/public/pics/' + pic_fold
+
+var file = require('fs').createWriteStream(gif_name);
+
+
+var pics = ['./photo_viewer/public/pics/09-08_gg_park/1567951224.jpg', './photo_viewer/public/pics/09-08_gg_park/1567951229.jpg', '../photo_viewer/public/pics/09-08_gg_park/1567951233.jpg'];
+
 
 gif.pipe(file);
-gif.setQuality(6);
-gif.setDelay(1000);
+gif.setQuality(3);
+gif.setDelay(500);
 gif.writeHeader();
 
+
+
+
+
+
+
 var addToGif = function(images, counter = 0) {
+
   getPixels(images[counter], function(err, pixels) {
 
     console.log('pixels = , ', pixels)
@@ -26,4 +49,28 @@ var addToGif = function(images, counter = 0) {
   })
 }
 
-addToGif(pics);
+
+
+get_pics(pics_path, first_pic_name, num_pics, ( gif_pics ) => {
+
+  pics = gif_pics;
+  let rotated_pics = [];
+
+  console.log('giffing pics', pics)
+  pics.forEach( (d) => {
+    rotate_pic(d, (rot_path) => {
+      rotated_pics.push(rot_path)
+      if(rotated_pics.length === pics.length) {
+        console.log('rotated_pics', rotated_pics)
+
+        addToGif(rotated_pics)
+      }
+    })
+  })
+  //addToGif(pics)
+
+})
+
+
+
+//addToGif(pics);
--- jpeg2gif/rotate_picture.js
+++ jpeg2gif/rotate_picture.js
@@ -1,7 +1,7 @@
 // thing to rotate a given pictures
 
 let Jimp = require('jimp')
-let rotate_pic = function(pic_path) {
+let rotate_pic = function(pic_path, cb) {
 
   Jimp.read(pic_path)
     .then(image => {
@@ -10,9 +10,14 @@ let rotate_pic = function(pic_path) {
     let newPath = './jpeg2gif/temp/' + pic_path.split('/')[ pic_path.split('/').length - 1]
 
     console.log("new pic should be at", newPath);
-
+    let tempPath = './jpeg2gif/temp/' + pic_path.split('/')[ pic_path.split('/').length - 1];
     image.rotate(90, true)
+      .crop(0, 0, 482, 600)
       .write( './jpeg2gif/temp/' + pic_path.split('/')[ pic_path.split('/').length - 1] )
+      if(cb) {
+        cb(tempPath)
+      }
+
   })
   .catch(err => {
     // Handle an exception.
